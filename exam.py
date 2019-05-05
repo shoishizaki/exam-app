d00cdeca44069e9b6319df5068f7e35b75b7e215
shoishizaki <shoishizaki1997@gmail.com>
2019-05-05 14:18:06 +0900

    Write __str__ function

--- exam.py
+++ exam.py
@@ -1,14 +1,23 @@
 class Exam():
-    def __init__(self, name, japanese, math, english, science, society):
-        self.name =name
+    def __init__(self, student, japanese, math, english, science, society):
+        self.student = student
         self.japanese = japanese
         self.math = math
         self.english = english
         self.science = science
         self.society = society
 
+    def __str__(self):
+        return 'name:{}, japanese:{},math:{}, english:{}, science:{}, society:{}'\
+            .format(self.student,
+                    self.japanese,
+                    self.math,
+                    self.english,
+                    self.science,
+                    self.society)
+
     def get_name(self):
-        return self.name
+        return self.student
 
     def get_japanese(self):
         return self.japanese
@@ -25,8 +34,8 @@ class Exam():
     def get_society(self):
         return self.society
 
-    def set_name(self, name):
-        self.name = name
+    def set_name(self, student):
+        self.student = student
 
     def set_japanese(self, japanese):
         self.japanese = japanese
