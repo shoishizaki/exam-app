7e9c068ee83aa55366a8982834135b86c0d588fb
shoishizaki <shoishizaki1997@gmail.com>
2019-05-05 14:17:41 +0900

    Write entry_exam and show_exam controller

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import student
 import student_list
+import exam_list
 
 def main():
     stu_ls = student_list.Student_list()
+    exam_ls = exam_list.Exam_list()
     while True:
         print('#####################')
         print(1, 'read student list')
         print(2, 'show student list')
-        print(3, 'quit app')
+        print(3, 'entry student exam')
+        print(4, 'show exam')
+        print(5, 'quit app')
         print('######################')
         number = input('choose number:')
 
@@ -16,6 +20,10 @@ def main():
         elif number == '2':
             stu_ls.show_student_list()
         elif number == '3':
+            exam_ls.enter_exam()
+        elif number == '4':
+            exam_ls.show_exam()
+        elif number == '5':
             print('Thank you.')
             break
         else:
