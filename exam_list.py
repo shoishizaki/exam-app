a687bd1d919845cf3ab624202ad01b1ff8e00871
shoishizaki <shoishizaki1997@gmail.com>
2019-05-05 14:21:59 +0900

    rewrite enter_exam and rewrite show_exam using ‘.format’

--- exam_list.py
+++ exam_list.py
@@ -1,27 +1,36 @@
 import exam
+import student_list
 
 class Exam_list():
     def __init__(self):
         self.exam_list = []
 
     def enter_exam(self):
+        students = student_list.Student_list()
+        students.read_exam()
         print("Please enter the student's score.")
-        name = input('name:')
-        japanese = input('japanese:')
-        math = input('math:')
-        english = input('english:')
-        science = input('science:')
-        society = input('society:')
-        ex = exam.Exam(name, japanese, math, english, science, society)
-        self.exam_list.append(ex)
+        for student in students.student_list:
+            print(student)
+            japanese = input('japanese:')
+            math = input('math:')
+            english = input('english:')
+            science = input('science:')
+            society = input('society:')
+            ex = exam.Exam(student, japanese, math, english, science, society)
+            self.exam_list.append(ex)
         print('Thank you!')
 
     def show_exam(self):
         if len(self.exam_list) == 0:
-            print('There is not exam data.')
+            print('There is no exam data here.')
 
         else:
             for ls in self.exam_list:
-                print('name:', ls.get_name(), ',', 'japanese:', ls.get_japanese(), ',',
-                      'math:', ls.get_math(), ',', 'english:', ls.get_english(), ',',
-                      'science:', ls.get_science(), ',', 'society:', ls.get_society())
+                print('student: {student}, japanese: {japanese}, math: {math},'
+                      ' english: {english}, science: {science},society: {society}'
+                      .format(student=ls.student,
+                              japanese=ls.get_japanese(),
+                              math=ls.get_math(),
+                              english=ls.get_english(),
+                              science=ls.get_science(),
+                              society=ls.get_society()))
