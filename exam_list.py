c4d57370a14277c325347d9eb51b7712f6e5444d
shoishizaki <shoishizaki1997@gmail.com>
2019-05-09 04:54:49 +0900

    Make calculation function

--- exam_list.py
+++ exam_list.py
@@ -1,9 +1,14 @@
 import exam
-import student_list
+import student
 
 class Exam_list():
     def __init__(self, students):
         self.exam_list = []
+        self.total_ls = []
+        self.science_subject_ls = []
+        self.cultural_subject_ls = []
+        self.successful_candidate_list = []
+        self.rejected_list = []
         self.enter_exam(students)
 
     def enter_exam(self, students):
@@ -38,3 +43,41 @@ class Exam_list():
                               english=ls.get_english(),
                               science=ls.get_science(),
                               society=ls.get_society()))
+
+    def cal_total_exam(self):
+        if len(self.exam_list) == 0:
+            print('There is no exam data here.')
+
+        else:
+            for i in range(len(self.exam_list)):
+                japanese = self.exam_list[i].get_japanese()
+                math = self.exam_list[i].get_math()
+                english = self.exam_list[i].get_english()
+                science = self.exam_list[i].get_science()
+                society = self.exam_list[i].get_society()
+                total = int(japanese) + int(math) + int(english) + int(science) + int(society)
+                self.total_ls.append(total)
+
+    def cal_science_exam(self):
+        if len(self.exam_list) == 0:
+            print('There is no exam data here.')
+
+        else:
+            for i in range(len(self.exam_list)):
+                math = self.exam_list[i].get_math()
+                english = self.exam_list[i].get_english()
+                science = self.exam_list[i].get_science()
+                total = int(math) + int(english) + int(science)
+                self.science_subject_ls.append(total)
+
+    def cal_cultural_exam(self):
+        if len(self.exam_list) == 0:
+            print('There is no exam data here.')
+
+        else:
+            for i in range(len(self.exam_list)):
+                japanese = self.exam_list[i].get_japanese()
+                english = self.exam_list[i].get_english()
+                society = self.exam_list[i].get_society()
+                total = int(japanese) + int(english) + int(society)
+                self.cultural_subject_ls.append(total)
